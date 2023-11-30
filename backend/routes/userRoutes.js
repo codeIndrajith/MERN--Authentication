12c4a7bc442383903fd7c986ca4ca136438f61f4
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-11-30 12:53:10 +0530

    Add request easilly using postman
    1. Authentication user
    2. Register user
    3. Logout user
    4. User Profile
    5. Update User Profile
    
    All the routes define and it works ..

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,20 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
+import {
+  authUser,
+  getUserProfile,
+  logoutUser,
+  registerUser,
+  updateUserProfile,
+} from '../controllers/userController.js';
 const router = express.Router();
 
+router.post('/', registerUser);
 router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
+
+// same way
+// router.get('/profile' , getUserProfile);
+// router.put('/profile' , updateUserProfile)
 
 export default router;
