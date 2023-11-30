3b6c17c77dab43cf49178c99486e95064583a0c6
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-11-30 14:37:14 +0530

    Setup the database using MongoDB
    Successfully connection into MongoDB database and connect to cloud and MongoDB compass

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import { notFound, errorHandler } from './middleware/erroMiddleware.js';
+import connectDB from './config/db.js';
 const PORT = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js';
 const app = express();
+
+// database connection
+connectDB();
 
 app.use('/api/users', userRoutes);
 
