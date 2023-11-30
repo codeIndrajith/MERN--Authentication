a44839c34745e27a366aa0f018d1ea3c3bfa5839
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-11-30 12:03:37 +0530

    Custom error middleware
    Handle the error using middleware.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 // If you add "type": "module" in package.json file. You will access to import syntax.
 
 import express from 'express';
-const PORT = 5000;
+import dotenv from 'dotenv';
+dotenv.config();
+import { notFound, errorHandler } from './middleware/erroMiddleware.js';
+const PORT = process.env.PORT || 5000;
+import userRoutes from './routes/userRoutes.js';
 const app = express();
 
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res) => res.send('Server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running to PORT ${PORT}`));
