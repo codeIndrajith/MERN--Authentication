0772200d183a0313140105192f577c73a77f11ee
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-12-01 10:41:12 +0530

    User logged out successfully
    User can logged out and then cookie is destroyed. how to check that. we set the cookie expires in 30 days. So user logged out we set the cookie expire date in 0. finally destroy the cookie.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,7 +63,13 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access  Public
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Logout User' });
+  // Destroy the cookie
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: 'User logged out' });
 });
 
 // @desc    Get user profile
