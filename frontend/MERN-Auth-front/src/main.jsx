205a26c5db611817e371c8c7c251325e1b106393
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2023-12-04 17:58:40 +0530

    State store the Redux
    The overall concept is to use Redux to manage the authentication-related state of the application. The authSlice.js file defines the initial state, actions, and reducers related to authentication. The store.js file then creates a Redux store with this authentication slice, making it available for use in a React application. The state changes (like setting credentials or logging out) are handled through Redux actions, and the state is stored in the Redux store and, for persistence, in the local storage of the browser. This allows components in the React application to read from and dispatch actions to this centralized state store, making it easier to manage and share state across different parts of the application

--- frontend/MERN-Auth-front/src/main.jsx
+++ frontend/MERN-Auth-front/src/main.jsx
@@ -7,6 +7,8 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
+import store from './store.js';
+import { Provider } from 'react-redux';
 import './index.css';
 import HomeScreen from './screens/HomeScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
@@ -23,7 +25,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
